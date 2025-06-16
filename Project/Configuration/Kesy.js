a69c6537ac3ddc0d5e56829c4114f68f06c6dc36
akshay-kumar-72 <akshaykumar725296@gmail.com>
2025-06-16 13:24:13 -0500

    Update Kesy.js

--- Project/Configuration/Kesy.js
+++ Project/Configuration/Kesy.js
@@ -1,1 +1,10 @@
+require('dotenv').config();
 
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  jwtSecret: process.env.JWT_SECRET,
+  google: {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  },
+};
