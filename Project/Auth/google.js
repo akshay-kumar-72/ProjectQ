471c496010a0364037f11c418e8c4c9c29fb378b
akshay-kumar-72 <akshaykumar725296@gmail.com>
2025-06-16 13:27:38 -0500

    Update google.js

--- Project/Auth/google.js
+++ Project/Auth/google.js
@@ -1,1 +1,24 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
+const keys = require('../config/keys');
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.google.clientID,
+      clientSecret: keys.google.clientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      let user = await User.findOne({ googleId: profile.id });
+      if (!user) {
+        user = await User.create({ googleId: profile.id, email: profile.emails[0].value });
+      }
+      done(null, user);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser((id, done) => User.findById(id).then(user => done(null, user)));
